50db830e719d9fd7617e0872466fb4fac451edef
Zachary Schutzman <zach@schutzman.net>
2018-07-23 17:40:54 -0400

    backup

--- polyominos/force/meta5.js
+++ polyominos/force/meta5.js
@@ -3,30 +3,27 @@ var width = 1200,
     height = 800,
     idno = 0;
     
+var w = width;
+var h = height;
+    
 var lx=-1;
 var ly=-1;
   
     
-    
-function dragstarted(d) {
-  d3.select(this).raise().classed("active", true);
-  console.log(d3.select(this).attr("x"));
-    
-    console.log(lx,ly,0);
-}
+var clsq = false;
 
-function dragged(d) {
-console.log(d3.event.x,d3.event.y);
 
-  d3.select(this).attr("transform", "translate(" + d3.event.x + (width/2 + 40) + "," + d3.event.y + (height/2) + ")");
-  
-}
+ var request = new XMLHttpRequest();
+   request.open("GET", "../dist_strings.json", false);
+   request.send(null)
+   var my_JSON_object = JSON.parse(request.responseText);
+   console.log(my_JSON_object);
 
-function dragended(d) {
-  d3.select(this).classed("active", false);
-  console.log("done");
-}
-
+var r_win_i = [0,0,0,0,0,0];
+var b_win_i = [0,0,0,0,0,0];
+var n_win_i = [0,0,0,0,0,0];
+var rwin = 0;
+var bwin=0;
 
 
 var tooltip = d3.select("body")
@@ -46,8 +43,6 @@ var tree = d3.tree()
     .size([2 * Math.PI, Math.min(width,height)/2.5])
     .separation(function(a, b) { return (a.parent == b.parent ? 1000 : 2000) / a.depth; });
 
-var stratify = d3.stratify()
-    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
 
     
 function mk_gr(fn,num) {
@@ -139,14 +134,15 @@ d3.json(fn, function(error, treeData) {
 
   node.append("rect").attr("width", 75)
       .attr("height", 75)
-      .style("fill","none")
+      .style("fill", function(d) {return get_col(d.data.str_rep.split('\n').join("").split(" ").join(""));})
       .style("stroke","#666")
       .style("stroke-width",2)
        .attr("x", -37)
       .attr("y", -37)
+      .attr("str_rep", function(d){ return d.data.str_rep.split('\n').join("").split(" ").join("");})
       .attr("idno", function(d) {return d.name;})
   node.append("image")
-      .attr("xlink:href", function(d) {console.log(d.data.name);return "m5-imgs/whole/im_"+d.data.name+".png";})
+      .attr("xlink:href", function(d) {return "m5-imgs/whole/im_"+d.data.name+".png";})
       .attr("x", -37)
       .attr("y", -37)
       .attr("width", 75)
@@ -173,7 +169,6 @@ mk_gr("m5-graphs/whole_trees2/g0.json",0);
 
 function swapgraph(){
     if (d3.event.defaultPrevented) return;
-    console.log(idno==d3.select(this).data()[0].data.name);
     if (idno == d3.select(this).data()[0].data.name) return;
               tooltip.style("visibility", "hidden");
   
@@ -190,6 +185,209 @@ function swapgraph(){
     svg.selectAll(".link").remove();
     svg.selectAll(".node").remove();
     mk_gr("m5-graphs/whole_trees2/g"+d3.select(this).data()[0].data.name+".json", d3.select(this).data()[0].data.name);
+    do_update(-1);
+    do_update2();
     
 }
 
+
+//////////////////////
+var grd = d3.select('body').append('svg')
+  .attr("width", width)
+  .attr("height",height);
+  
+  
+var chk = "";
+var dist1 = 0;
+var dist2 = 0;
+var dist3 = 0;
+var dist4 = 0;
+var dist5 = 0;
+var cnt = 0;  
+  
+
+var simp_fill = ['#0000ff','#808080','#ff0000'];
+
+// calculate number of rows and columns
+var squaresRow = 5;
+var squaresColumn = 5;
+var square=70;
+
+// loop over number of columns
+_.times(squaresColumn, function(n) {
+
+  // create each set of rows
+  var rows = grd.selectAll('rect' + ' .row-' + (n + 1))
+    .data(d3.range(squaresRow))
+    .enter().append('rect')
+    
+
+    
+    
+    
+    
+    .attr("class", function(d, i) {return 'square row-' + (n + 1) + ' ' + 'col-' + (i + 1);})
+    .attr("id", function(d, i) {
+        return 's-' + (n + 1) + (i + 1);
+    })
+      .attr("width", square)
+      .attr("height",square)
+      .attr("x", function(d, i) {
+        return (i * 1.07*square);
+      })
+      .attr("y", (n * 1.07*square))
+    
+    .attr("party",0)
+    .style("fill",simp_fill[1])
+    .style("stroke","#555")
+    .style("stroke-width",5)
+    
+    
+    
+    .on("mouseover",function(d){
+        d3.select(this).style("stroke","#000");
+        d3.select(this).style("stroke-width","6");
+    })
+    
+    .on("mouseout", function(d){
+        d3.select(this).style("stroke","#555");
+        d3.select(this).style("stroke-width","5")
+    })
+    
+    .on("click", function(d){
+     clsq = true;
+     do_update(this);
+     do_update2();
+    }
+    );
+
+
+});
+    
+    
+function do_update(r){
+            console.log("YAAA");
+   
+    //if (d3.event != null && d3.event.defaultPrevented) return;
+        if (d3.event != null && r != -1){
+            var t = parseInt(d3.select(r).attr("party"));
+            d3.select(r).attr("party", t+1);
+            if (d3.select(r).attr("party") == 2){d3.select(r).attr("party",-1);}
+        }
+  
+
+        console.log("YAAA");
+
+    grd.selectAll('rect').each(function(d){
+        //console.log(d3.select(this).attr("party"));
+        if (d3.select(this).attr("party") == 0) d3.select(this).style("fill", simp_fill[1]);
+        if (d3.select(this).attr("party") == 1) d3.select(this).style("fill", simp_fill[2]);
+        if (d3.select(this).attr("party") == -1) d3.select(this).style("fill", simp_fill[0]);
+
+    })
+
+        r_win_i = [0,0,0,0,0,0];
+        b_win_i = [0,0,0,0,0,0];
+        n_win_i = [0,0,0,0,0,0];
+
+
+        console.log("HHH");
+}
+function do_update2(){
+        g.selectAll("rect").each(function(d){
+            var hld = this;
+            chk = d3.select(this).attr("str_rep");
+   
+    
+        dist1=0;
+        dist2=0;
+        dist3=0;
+        dist4=0;
+        dist5=0;
+        cnt=0;
+        console.log("YAAA");
+
+        grd.selectAll("rect").each(function(e){
+            rwin = 0;
+bwin = 0;
+            var b = parseInt(d3.select(this).attr("party"));
+            if (chk[cnt] == 1){
+            dist1 = dist1 + b;
+            } else  if (chk[cnt] == 2){
+            dist2 = dist2 + b;
+            }else   if (chk[cnt] == 3){
+            dist3 = dist3 + b;
+            }else   if (chk[cnt] == 4){
+            dist4 = dist4 + b;
+            }else   if (chk[cnt] == 5){
+            dist5 = dist5 + b;
+                       }
+            cnt = cnt+1;
+
+        });
+        dist1 = Math.sign(dist1);
+        dist2 = Math.sign(dist2);
+        dist3 = Math.sign(dist3);
+        dist4 = Math.sign(dist4);
+        dist5 = Math.sign(dist5);
+        
+        if (dist1 > 0) {rwin +=1;}
+        if (dist2 > 0) {rwin +=1;}
+        if (dist3 > 0) {rwin +=1;}
+        if (dist4 > 0) {rwin +=1;}
+        if (dist5 > 0) {rwin +=1;}
+        if (dist1 < 0) {bwin +=1;}
+        if (dist2 < 0) {bwin +=1;}
+        if (dist3 < 0) {bwin +=1;}
+        if (dist4 < 0) {bwin +=1;}
+        if (dist5 < 0) {bwin +=1;}
+        
+        r_win_i[parseInt(rwin)] += 1;
+        b_win_i[parseInt(bwin)] += 1;
+        var col = Math.sign(dist1 + dist2 + dist3 + dist4 + dist5) + 1;
+        d3.select(this).style("fill", simp_fill[col]);
+        });
+    clsq = false;
+    console.log(r_win_i,b_win_i);
+    }
+    
+function get_col(chkstr){
+        var chk = chkstr;
+        dist1=0;
+        dist2=0;
+        dist3=0;
+        dist4=0;
+        dist5=0;
+        cnt=0;
+        console.log("YAAA");
+
+        grd.selectAll("rect").each(function(e){
+            rwin = 0;
+bwin = 0;
+            var b = parseInt(d3.select(this).attr("party"));
+            if (chk[cnt] == 1){
+            dist1 = dist1 + b;
+            } else  if (chk[cnt] == 2){
+            dist2 = dist2 + b;
+            }else   if (chk[cnt] == 3){
+            dist3 = dist3 + b;
+            }else   if (chk[cnt] == 4){
+            dist4 = dist4 + b;
+            }else   if (chk[cnt] == 5){
+            dist5 = dist5 + b;
+                       }
+            cnt = cnt+1;
+
+        });
+        dist1 = Math.sign(dist1);
+        dist2 = Math.sign(dist2);
+        dist3 = Math.sign(dist3);
+        dist4 = Math.sign(dist4);
+        dist5 = Math.sign(dist5);
+        
+
+        var col = Math.sign(dist1 + dist2 + dist3 + dist4 + dist5) + 1;
+        return simp_fill[col];
+    
+    
+}
